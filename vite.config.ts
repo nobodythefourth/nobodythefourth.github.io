3b60db07abbe0db8dbb46cd81e73e10f49abfc88
nobodythefourth <nobody.the.fourth@gmail.com>
2025-04-24 15:41:31 -0700

    fix: set vite base to /playground/ for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/website/',
+  base: '/playground/',
   server: {
     host: "::",
     port: 8080,
